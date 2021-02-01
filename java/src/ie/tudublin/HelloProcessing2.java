6e587380e5289a08fd54ec1d6b1c2d0bc1c0c98f
rossmcel <rossmcelhinney123@gmail.com>
2021-02-01 14:09:24 +0000

    Week 2 Lecture 2

--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -16,8 +16,23 @@ public class HelloProcessing2 extends PApplet
 	
 	public void draw()
 	{	
-		background(0);
-		stroke(255);
-		line(10, 10, 100, 100);
+		background(232, 14, 2); // background colou
+		stroke(255); // pen colout = white
+
+		// outer circle
+		fill(255, 251, 0);
+		ellipse(250, 220, 220, 220); // circle - cx, cy, w, h
+
+		// triangle
+		fill(29, 242, 242);
+		triangle(250, 100, 130, 300, 370, 300);
+
+		// inner circle
+		fill(181, 181, 177);
+		ellipse(250, 200, 100, 50); // circle - cx, cy, w, h
+
+		// inner inner circle
+		fill(5, 5, 5);
+		ellipse(250, 200, 35, 35); // circle - cx, cy, w, h
 	}
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,37 +2,7 @@ package ie.tudublin;
 
 public class Main
 {
-    public void catsAndDogs()
-    {
-        System.out.println("Hello world");
-
-        Animal misty = new Dog("Misty");
-
-        Animal topCat = new Cat("TopCat");
-
-        System.out.println(misty);
-        System.out.println(topCat);
-
-        misty = topCat;
-
-        topCat.setName("Garfield");
-
-        System.out.println(misty);
-        System.out.println(topCat);
-
-        // What will get printed out??
-
-        // 1. topcat, Garfield
-        // 2. garfield, Garfield 
-
-        Cat ginger = new Cat("Ginger");
-        while(ginger.getNumLives() > 0)
-        {
-            ginger.kill();
-        }
-        ginger.kill();
-    }
-
+    
     public void helloProcessing1()
     {
         String[] a = {"MAIN"};
